4e2722acb33c90f8deb442b4060e8f566a18f63b
Darlene Zouras <dzouras@gmail.com>
2020-06-12 19:59:46 -0500

    added global styles (colors, fonts) and vue router

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,28 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <Header />
+    <Main />
+
+    HI DOES THIS WORK
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import Main from '@/components/Main.vue'
+import Header from '@/components/Header.vue'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    Main,
+    Header
   }
 }
 </script>
 
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+<style lang="scss">
+  @import "../public/scss/global.scss";
+  #app {
+    
+  }
 </style>
---
+++ src/components/Header.vue
@@ -0,0 +1,15 @@
+<template>
+    <header>
+        <p>This is the header</p>
+    </header>
+</template>
+
+<style lang="scss">
+    
+</style>
+
+<script>
+export default {
+    name: 'Header'
+}
+</script>
