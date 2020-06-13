58c1fb0be248047e6f4be42f33e7f4be7351a842
Darlene Zouras <dzouras@gmail.com>
2020-06-12 21:56:04 -0500

    Added message on main

--- src/components/Main.vue
+++ src/components/Main.vue
@@ -1,5 +1,9 @@
 <template>
-  <Title type="h1" title="Open-RP" />
+  <div>
+    <Title type="h1" title="Open-RP" />
+
+    <p>Thanks for visiting- this is an in-progress project by <a href="https://darzouras.com">Darlene Zouras</a>. If you're interested in helping you can visit the repo <a href="https://github.com/darzouras/open-rp">here</a>.</p>
+  </div>
 </template>
 
 <script>
