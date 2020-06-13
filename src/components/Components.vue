b4e27e7d2fa5e07092324acabecc4c91ede41759
Darlene Zouras <dzouras@gmail.com>
2020-06-12 21:48:49 -0500

    Made my first style component

--- src/components/Components.vue
+++ src/components/Components.vue
@@ -1,15 +1,29 @@
 <template>
-    <div>
-        <h1>Components</h1>
-    </div>
+    <main>
+        <Title type="h1" title="Components" />
+        <article>
+            <Title type="h2" title="Big Title" />
+        </article>
+    </main>
 </template>
 
-<style lang="scss">
+<style lang="scss" scoped>
+
+    main {
+        width: 90%;
+        max-width: 800px;
+        margin: auto;
+    }
     
 </style>
 
 <script>
+import Title from '@/components/Title.vue'
+
 export default {
-    name: 'Components'
+    name: 'Components',
+    components: {
+        Title
+    }
 }
 </script>
---
+++ src/components/Title.vue
@@ -0,0 +1,21 @@
+<template>
+  <component :is="type" class="title-large">{{title}}</component>
+</template>
+
+<style lang="scss" scoped>
+    .title-large {
+        font-size: 2.5rem;
+        letter-spacing: .1rem;
+        font-weight: 900;
+    }
+</style>
+
+<script>
+export default {
+  name: 'Title',
+  props: {
+    type: String,
+    title: String
+  }
+}
+</script>
