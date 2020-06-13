30ac6d6f933ccd56305c4e8f8e172267ec7a86ad
Darlene Zouras <dzouras@gmail.com>
2020-06-12 23:18:44 -0500

    Created input component

--- src/components/Components.vue
+++ src/components/Components.vue
@@ -4,6 +4,10 @@
         <article>
             <Title type="h2" title="Big Title" />
         </article>
+
+        <article>
+            <Input label="This is the label" name="input-name" placeholder="Input placeholder"/>
+        </article>
     </div>
 </template>
 
@@ -13,11 +17,13 @@
 
 <script>
 import Title from '@/components/Title.vue'
+import Input from '@/components/Input.vue'
 
 export default {
     name: 'Components',
     components: {
-        Title
+        Title,
+        Input
     }
 }
 </script>
---
+++ src/components/Input.vue
@@ -0,0 +1,43 @@
+<template>
+    <label v-bind:class="{ error: error }">
+        {{ label }}
+        <input type="text" :name="name" :id="name" :placeholder="placeholder">
+    </label>
+</template>
+
+<style lang="scss" scoped>
+    @import "../../public/scss/global.scss";
+
+    label {
+        font-size: .8rem;
+        font-weight: 100;
+        
+        input {
+            display: block;
+            width: calc(100% - 1rem - 2px);
+            border: 1px solid $lightblue;
+            border-radius: 5px;
+            padding: .5rem;
+            font-size: 1rem;
+            color: $darkblue;
+        }
+
+        &.error {
+            input {
+                border-color: $red;
+            }
+        }
+    }
+    
+</style>
+
+<script>
+export default {
+  name: 'Input',
+  props: {
+    label: String,
+    name: String,
+    placeholder: String
+  }
+}
+</script>
