5d422448bc0a7b999b8dd76c166e800182ea85ab
Darlene Zouras <dzouras@gmail.com>
2020-06-12 21:32:30 -0500

    Header and nav working

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -1,24 +1,46 @@
 <template>
     <header>
-        <nav>
-            <ul>
-                <li>
-                    <router-link to="/">Home</router-link>
-                </li>
-                <li>
-                    <router-link to="/components">Components</router-link>
-                </li>
-            </ul>
-        </nav>
+        <router-link to="/"><div class="header-title">
+            Open-RP
+        </div></router-link>
+
+        <Nav />
     </header>
 </template>
 
-<style lang="scss">
-    
+<style lang="scss" scoped>
+    @import "../../public/scss/global.scss";
+
+    header {
+        box-shadow: 0px 5px 10px $shadow;
+        display: flex;
+        align-items: center;
+        justify-content: space-between;
+        padding: 10px 5%;
+        width: 90%;
+
+        .header-title {
+            font-weight: 150;
+            text-transform: lowercase;
+            font-size: 1.5rem;
+            padding: .5rem 1rem;
+            background: $green;
+            color: white;
+
+            a {
+                color: white;
+                text-decoration: none;
+            }
+        }
+    }
 </style>
 
 <script>
+import Nav from './Nav.vue'
 export default {
-    name: 'Header'
+    name: 'Header',
+    components: {
+        Nav
+    }
 }
 </script>
---
+++ src/components/Nav.vue
@@ -0,0 +1,44 @@
+<template>
+    <nav>
+        <ul id="nav-links">
+            <li v-for="link in links" :key="link.name">
+                <router-link :to="link.link">{{link.name}}</router-link>
+            </li>
+        </ul>
+    </nav>
+</template>
+
+<style lang="scss" scoped>
+    nav {
+        ul {
+            list-style: none;
+            display: flex;
+            align-items: center;
+
+            li {
+                margin: 0 5px;
+                padding: 0;
+            }
+        }
+    }
+</style>
+
+<script>
+    export default {
+        name: 'Nav',
+        data() {
+            return {
+                links: [
+                    {       
+                        name: 'Home',
+                        link: '/'
+                    },
+                    {
+                        name: 'Components',
+                        link: '/components'
+                    }
+                ]
+            }
+        }
+    }
+</script>
