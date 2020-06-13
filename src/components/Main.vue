4c806895cbd31ec0d0f4aadfe2f3111e0e6c8100
Darlene Zouras <dzouras@gmail.com>
2020-06-13 11:27:32 -0500

    accessibility updated

--- src/components/Main.vue
+++ src/components/Main.vue
@@ -2,7 +2,7 @@
   <div>
     <Title type="h1" title="Open-RP" />
 
-    <p>Thanks for visiting- this is an in-progress project by <a href="https://darzouras.com">Darlene Zouras</a>. If you're interested in helping you can visit the repo <a href="https://github.com/darzouras/open-rp">here</a>.</p>
+    <p>Thanks for visiting- this is an in-progress project by <a href="https://darzouras.com">Darlene Zouras</a>. If you're interested in helping you can <a href="https://github.com/darzouras/open-rp">see the project on Github</a>.</p>
   </div>
 </template>
 
