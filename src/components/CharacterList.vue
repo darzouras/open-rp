2cff69d48c266ab7c7e0bf50c233cd1ac445695b
Darlene Zouras <dzouras@gmail.com>
2020-06-14 10:30:13 -0500

    Created character list component

---
+++ src/components/CharacterList.vue
@@ -0,0 +1,42 @@
+<template>
+    <ul>
+        <li v-for="character in data" :key="character.name">
+            <p>{{ character.name }} || {{ character['.key'] }}</p>
+            <button @click="deleteCharacter(character)">Remove</button>
+        </li>
+    </ul>
+</template>
+
+<script>
+import { mapGetters } from 'vuex'
+import { db } from '../main'
+import * as firebase from 'firebase'
+
+export default {
+    name: 'CharacterList',
+    props: {
+        data: Array
+    },
+    firestore() {
+        return {
+            characters: db.collection('characters'),
+            users: db.collection('users')
+        }
+    },
+    methods: {
+        deleteCharacter: function(character) {
+            this.$firestore.users.doc(this.user.data.displayName).update(
+                {
+                    characters: firebase.firestore.FieldValue.arrayRemove(this.$firestore.characters.doc(character['.key']))
+                }
+            )
+            this.$firestore.characters.doc(character['.key']).delete();
+        }
+    },
+    computed: {
+        ...mapGetters({
+            user: 'user'
+        })
+    }
+}
+</script>
--- src/routes/Dashboard.vue
+++ src/routes/Dashboard.vue
@@ -4,28 +4,26 @@
 
         <p v-if="user.loggedIn">{{user.data.displayName}} logged in!</p>
 
+        <Title type="h2" title="Your Characters" />
+
         <router-link to="/new-character">Add a new character</router-link>
 
-        <ul>
-            <li v-for="character in characters" :key="character.name">
-                <p>{{ character.name }} || {{ character['.key'] }}</p>
-                <button @click="deleteCharacter(character)">Remove</button>
-            </li>
-        </ul>
+        <CharacterList :data="characters" />
     </div>
 </template>
 
 <script>
 import { mapGetters } from 'vuex' 
 import { db } from '../main'
-import * as firebase from 'firebase'
 
 import Title from '@/components/Title.vue'
+import CharacterList from '@/components/CharacterList.vue'
 
 export default {
     name: 'Dashboard',
     components: {
-        Title
+        Title,
+        CharacterList
     },
     data() {
         return {
@@ -37,16 +35,6 @@ export default {
         return {
             characters: db.collection('characters'),
             users: db.collection('users')
-        }
-    },
-    methods: {
-        deleteCharacter: function(character) {
-            this.$firestore.users.doc(this.user.data.displayName).update(
-                {
-                    characters: firebase.firestore.FieldValue.arrayRemove(this.$firestore.characters.doc(character['.key']))
-                }
-            )
-            this.$firestore.characters.doc(character['.key']).delete();
         }
     },
     computed: {
