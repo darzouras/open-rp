2f7e71fff9d0a9e636860e28b7a98119ced6ccbf
Darlene Zouras <dzouras@gmail.com>
2020-06-14 21:54:49 -0500

    Added vuex-persistedstate and reloading doesn't lose user state anymore

--- src/store.js
+++ src/store.js
@@ -1,7 +1,10 @@
 import Vue from "vue";
 import Vuex from "vuex";
+import createPersistedState from 'vuex-persistedstate'
+
 Vue.use(Vuex);
 export default new Vuex.Store({
+  plugins: [createPersistedState()],
   state: {
     user: {
       loggedIn: false,
