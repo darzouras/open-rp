d5884ff2d19c1b5a56ec6e993d605aa208fe92bc
Darlene Zouras <dzouras@gmail.com>
2020-06-14 21:23:30 -0500

    replaced import * from firebase in characterlist component

--- src/components/CharacterList.vue
+++ src/components/CharacterList.vue
@@ -10,7 +10,7 @@
 <script>
 import { mapGetters } from 'vuex'
 import { db } from '../main'
-import * as firebase from 'firebase'
+import firebase from 'firebase'
 
 export default {
     name: 'CharacterList',
