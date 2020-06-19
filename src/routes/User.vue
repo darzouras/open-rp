8d4185047ed753d19b42bb3d6492e5166fb76a70
Darlene Zouras <dzouras@gmail.com>
2020-06-19 15:38:41 -0500

    Fixed consecutive loading of components

--- src/routes/User.vue
+++ src/routes/User.vue
@@ -1,11 +1,11 @@
 <template>
     <div class="user-wrapper">
-        <Title type="h1" :title="$route.params.user" />
+        <Title type="h1" :title="player" />
 
         <section>
             <TitleMed type="h2">Characters</TitleMed>
 
-            <CharacterList v-if="characters.length > 0" :data="characters" />
+            <CharacterList v-if="userChars.length > 0" :data="userChars" />
         </section>
     </div>
 </template>
@@ -26,12 +26,33 @@ export default {
     },
     data() {
         return {
-            characters: []
+            characters: [],
+            userChars: [],
+            player: this.$route.params.user
         }
     },
     firestore() {
         return {
-            characters: db.collection('characters').where('user', '==', this.$route.params.user)
+            userChars: db.collection('characters').where('user', '==', this.player),
+            characters: db.collection('characters')
+        }
+    },
+    watch: {
+        $route(to) {
+            this.player = to.params.user
+
+            this.$firestore.characters.where('user', '==', this.player).get().then(snapshot => {
+                this.userChars = [];
+                snapshot.docs.forEach(val => {
+                    // need to do some weird json rebuilding here since this doesn't include the id or [.key] by default
+                    var charData = {}
+                    charData['.key'] = val.id
+                    charData = Object.assign({}, charData, val.data())
+                    this.userChars.push(charData)
+                })
+            }).catch(err =>{
+                console.log(err.message)
+            })
         }
     }
 }
