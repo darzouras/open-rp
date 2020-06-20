46bf654eb7cf9a87d8ba67877018d962b2d18d28
Darlene Zouras <dzouras@gmail.com>
2020-06-19 19:18:45 -0500

    footer style change

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -35,7 +35,7 @@
         margin: 5rem 0 0;
         padding: 3rem 5% 7rem;
         width: 90%;
-        background: $green;
+        background: $darkblue;
         color: white;
 
         .footer-nav {
