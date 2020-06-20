d8ca5605f8c34ffc44bbe89a8b43600ba76f239c
Darlene Zouras <dzouras@gmail.com>
2020-06-20 09:30:55 -0500

    Added capability to update and view player info

--- src/components/PlayerInfo.vue
+++ src/components/PlayerInfo.vue
@@ -1,23 +1,61 @@
 <template>
-    <div class="info-wrapper">
+    <div class="info-wrapper" v-if="data">
         <table>
             <tr v-if="data.name">
-                <th>Name</th>
+                <th scope="row">Name</th>
                 <td>{{ data.name }}</td>
             </tr>
+            <tr v-if="data.birthday">
+                <th scope="row">Birthday</th>
+                <td>{{ data.birthday }}</td>
+            </tr>
+            <tr v-if="data.pronouns">
+                <th scope="row">Pronouns</th>
+                <td>{{ data.pronouns }}</td>
+            </tr>
+            <tr v-if="data.timezone">
+                <th scope="row">Time zone</th>
+                <td>{{ data.timezone }}</td>
+            </tr>
             <tr v-if="data.twitter">
-                <th>Twitter</th>
+                <th scope="row">Twitter</th>
                 <td>
                     <a v-bind:href="'https://twitter.com/' + data.twitter">{{ data.twitter }}</a>
                 </td>
             </tr>
             <tr v-if="data.discord">
-                <th>Discord</th>
+                <th scope="row">Discord</th>
                 <td>
                     {{ data.discord }}
                 </td>
             </tr>
+            <tr v-if="data.plurk">
+                <th scope="row">Plurk</th>
+                <td>
+                    <a v-bind:href="'https://plurk.com/' + data.plurk">{{ data.plurk }}</a>
+                </td>
+            </tr>
+            <tr v-if="data.tumblr">
+                <th scope="row">Tumblr</th>
+                <td>
+                    <a v-bind:href="'https://' + data.tumblr + '.tumblr.com'">{{ data.tumblr }}</a>
+                </td>
+            </tr>
+            <tr v-if="data.livejournal">
+                <th scope="row">Livejournal</th>
+                <td>
+                    <a v-bind:ref="'https://' + data.livejournal + '.livejournal.com'">{{ data.livejournal }}</a>
+                </td>
+            </tr>
+            <tr v-if="data.dreamwidth">
+                <th scope="row">Dreamwidth</th>
+                <td>
+                    <a v-bind:href="'https://' + data.dreamwidth + '.dreamwidth.org'">{{ data.dreamwidth }}</a>
+                </td>
+            </tr>
         </table>
+
+        <div class="info" v-html="data.extra"></div>
     </div>
 </template>
 
@@ -30,15 +68,23 @@
         border-radius: 15px;
         padding: 1rem;
         width: calc(100% - 2rem - 4px);
-        margin: 1rem 0;
+        margin: 1rem 0 2rem;
 
         table {
             text-align: left;
             width: 100%;
 
             th {
-                width: 5rem;
+                width: 6rem;
             }
+
+            th, td {
+                padding: .25rem 0;
+            }
+        }
+
+        .info {
+            padding: 1rem 0 0;
         }
 
         @media (min-width: 768px) {
--- src/routes/UpdateProfile.vue
+++ src/routes/UpdateProfile.vue
@@ -2,12 +2,30 @@
     <div class="update-wrapper">
         <Title type="h1">Update Your Profile</Title>
 
+        <p>All fields are optional but all filled fields will appear on your user info page.</p>
+
         <form action="#" @submit.prevent="updateUserData">
             <Input label="Name" type="name" name="name" v-model="userData.name" />
 
-            <Input label="Twitter account name" type="text" name="twitter" v-model="userData.twitter" />
+            <Input label="Birthday" note="required if you want to participate in 18+ chats" type="date" name="birthday" v-model="userData.birthday" />
 
-            <Input label="Discord username" type="text" name="discord" v-model="userData.discord" />
+            <Input label="Pronouns" type="text" name="name" v-model="userData.pronouns" />
+
+            <Input label="Time zone" name="Use GMT+/-" type="text" v-model="userData.timezone" />
+
+            <Input label="Twitter account name" note="username only" type="text" name="twitter" v-model="userData.twitter" />
+
+            <Input label="Discord username" note="Enter format DisplayName#0000" type="text" name="discord" v-model="userData.discord" />
+
+            <Input label="Plurk user name" note="username only" type="text" name="plurk" v-model="userData.plurk" />
+            
+            <Input label="Tumblr username" note="blog profile name only" type="text" name="tumblr" v-model="userData.tumblr" />
+
+            <Input label="Livejournal username" note="lj profile name only" type="text" name="livejournal" v-model="userData.livejournal" />
+
+            <Input label="Dreamwidth username" note="dw profile name only" type="text" name="tumblr" v-model="userData.dreamwidth" />
+
+            <Input label="Other info" note="Basic html allowed here" type="textarea" name="extra" v-model="userData.extra" />
 
             <Button type="submit">Submit Update</Button>
         </form>
@@ -35,8 +53,16 @@ export default {
         return {
             userData: {
                 name: '',
+                birthday: '',
+                pronouns: '',
+                timezone: '',
                 twitter: '',
+                plurk: '',
                 discord: '',
+                tumblr: '',
+                livejournal: '',
+                dreamwidth: '',
+                extra: ''
             },
             error: null,
             success: false
@@ -55,9 +81,17 @@ export default {
         },
         updateUserData: function() {
             this.$firestore.users.doc(this.user.data.displayName).update({
-                name: this.userData.name,
-                twitter: this.userData.twitter,
-                discord: this.userData.discord
+                name: this.userData.name || '',
+                birthday: this.userData.birthday || '',
+                pronouns: this.userData.pronouns || '',
+                timezone: this.userData.timezone || '',
+                twitter: this.userData.twitter || '',
+                plurk: this.userData.plurk || '',
+                discord: this.userData.discord || '',
+                tumblr: this.userData.tumblr || '',
+                livejournal: this.userData.livejournal || '',
+                dreamwidth: this.userData.dreamwidth || '',
+                extra: this.$sanitize(this.userData.extra) || ''
             }).then(() => {
                 this.success = true
             }).catch(err => {
